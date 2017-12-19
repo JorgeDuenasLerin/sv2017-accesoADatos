01130cca04488dcc687c7edf7533cf0fa299b414
Nacho Cabanes <nacho@nachocabanes.com>
2017-12-19 21:03:56 +0100

    Contacto con PHP 02: Incrustando código PHP

---
+++ tema02-accesoABasesDeDatos/php/02/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Ejercicio 2</title>
+		<meta charset="utf-8" />
+	</head>
+	
+	<body>
+		<h1>Ejercicio 2</h1>
+		<p>
+			<?php
+			echo "Hola, mundo";
+			?>
+		</p>
+	</body>
+</html>
