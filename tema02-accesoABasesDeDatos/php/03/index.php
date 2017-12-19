982ec7afc40cc678492ad8b6019bf4e4d1fa8ca7
Nacho Cabanes <nacho@nachocabanes.com>
2017-12-19 21:04:25 +0100

    Contacto con PHP 03: Variables, for, concatenar

---
+++ tema02-accesoABasesDeDatos/php/03/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Ejercicio 3</title>
+		<meta charset="utf-8" />
+	</head>
+	
+	<body>
+		<h1>Ejercicio 3</h1>
+		<p>
+			<?php
+			for($i=0; $i<100; $i++)
+				echo "Hola, mundo " . $i . " - ";
+			?>
+		</p>
+	</body>
+</html>
