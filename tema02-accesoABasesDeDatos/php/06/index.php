5e8bc6260980facb0d15328c676e05655c808229
Nacho Cabanes <nacho@nachocabanes.com>
2017-12-26 15:14:35 +0100

    Contacto con PHP 06: Comprobar si se reciben datos del formulario

---
+++ tema02-accesoABasesDeDatos/php/06/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ejercicio 6</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<h1>Ejercicio 6</h1>
+		<form action="resultados.php" method="POST">
+			Introduzca parte del nombre de la receta:<br>
+			<input type="text" name="nombre"><br>
+			<input type="submit" value="Buscar">
+		</form>
+	
+	</body>
+</html>
